71babbb3e652718a2aa7b8df75d2a165f1b9750a
Brasero <brandonricci4@gmail.com>
2021-08-17 16:33:36 +0200

    passage js

---
+++ src/components/ButtonConnexion.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ButtonConnexion () {
+
+    return (
+
+        <button type="submit" className="beta-connect-button">
+            Se connecter
+        </button>
+
+    )
+}
+
+export default ButtonConnexion
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButtonConnexion from './ButtonConnexion'
+
+function NavBar() {
+    
+    return (
+
+        <div id="beta-nav-bar">
+            <ButtonConnexion />
+        </div>
+    )
+}
+
+export default NavBar
